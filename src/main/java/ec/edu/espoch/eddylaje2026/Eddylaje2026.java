6d2580238e88567a3bf30e901d198e3a2c7fa6cd
Zanyn-user <zanynuser@gmail.com>
2024-11-07 13:40:16 -0500

    primer comentario

---
+++ src/main/java/ec/edu/espoch/eddylaje2026/Eddylaje2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.eddylaje2026;
+
+/**
+ *
+ * @author User
+ */
+public class Eddylaje2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
